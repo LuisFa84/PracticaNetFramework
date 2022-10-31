eff90b1da0fd3baf75398f09927ddd41b68c46ef
Luisfa84 <lfalvarez@outlook.com.ar>
2022-10-30 22:59:45 -0300

    Cambios

---
+++ PracticaNetFramework/Pages/About_Us.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PracticaNetFramework.Pages
+{
+    public class About_UsModel : PageModel
+    {
+        private readonly ILogger<About_UsModel> _logger;
+
+        public About_UsModel(ILogger<About_UsModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ PracticaNetFramework/Pages/Productos.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PracticaNetFramework.Pages
+{
+    public class ProductosModel : PageModel
+    {
+        private readonly ILogger<ProductosModel> _logger;
+
+        public ProductosModel(ILogger<ProductosModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
